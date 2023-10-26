10df5b8070659fc36e6d0d0031848d6a83a43b64
MaksikDziobak <maksymilian.dziobak@icloud.com>
2023-10-26 20:03:54 +0200

    Pliki

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{   
+    private float diamonds = 0f;
+
+    [SerializeField] private Text diamondsText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Diamond"))
+        {
+            Destroy(collision.gameObject);
+            diamonds++;
+            diamondsText.text = "Diamonds: " + diamonds;
+        }
+    }
+}
